d84c0c20fea072a5ebf3c4c67e6684d105a6606b
John Doe <johndoe@example.com>
2021-12-31 21:40:15 +0100

    modifier le contenu du dossier BriefAgenceLocationToutVehicule-2

--- BriefAgenceLocationToutVehicule-2/script_reserve.js
+++ BriefAgenceLocationToutVehicule-2/script_reserve.js
@@ -1,9 +1,9 @@
+//---
 const seclectElmtBoiteVitesse = document.querySelector('#boite_vitesse');
-// const moto = document.querySelector("#moto");
 
 boite_vitesse.addEventListener('change', function() {
     // Les élément du bloc liste_carburant
-    var liste_carburant = document.querySelector('#liste_carburant');
+    let liste_carburant = document.querySelector('#liste_carburant');
     var liste_carburant_pour_moto = document.querySelector('#liste_carburant_pour_moto');
     
     // Les éléments du bloc liste_vehicule
@@ -20,6 +20,11 @@ boite_vitesse.addEventListener('change', function() {
         liste_carburant_pour_moto.style.display = 'block';
         //
         liste_vehicule.style.display = 'none';
+        liste_vehicule_pour_manuelle.style.display = 'none';
+        liste_vehicule_pour_automatique.style.display = 'none';
+        //
+        listeCarburantPourCitadine.style.display = 'none';
+        console.log("listeCarburantPourCitadine.style.display = ", listeCarburantPourCitadine.style.display);
     }
     else if (valeurSelectionnee == "manuelle") // Afficher seulement le bloc de la liste vehicule pour manuelle
     {
@@ -44,36 +49,109 @@ boite_vitesse.addEventListener('change', function() {
         liste_vehicule_pour_manuelle.style.display = 'none';
     }
     else {
+        liste_carburant_pour_moto.style.display = 'none';
         liste_carburant.style.display = 'none';
     }
 });
 
+//---
 const seclectElmtType_vehicule_m = document.querySelector('#type_vehicule_m');
-
+var listeCarburantPourCitadine;
 seclectElmtType_vehicule_m.addEventListener('change', function(){
-    // Les élément du bloc de 'liste des véhicules manuelle'
-
+    // Les élément du bloc liste_carburant
+    let liste_carburant = document.querySelector('#liste_carburant');
+    // Les élément du bloc de 'liste des véhicules manuelle' pour Citadine
+    listeCarburantPourCitadine = document.querySelector("#liste_carburant_pour_citadine");
+    // Les élément du bloc de 'liste des véhicules manuelle' pour Compact
+    var listeCarburantPourCompact = document.querySelector("#liste_carburant_pour_compact");
+    // Les élément du bloc de 'liste des véhicules manuelle' pour Utilitaire
+    var listeCarburantPourUtilitaire = document.querySelector("#liste_carburant_pour_utilitaire");
+    // Les élément du bloc de 'liste des véhicules manuelle' pour Engin de Chantier
+    var listeCarburantPourEnginChantier = document.querySelector("#liste_carburant_pour_engin_chantier");
 
     let valeurSelectionnee = seclectElmtType_vehicule_m.options[seclectElmtType_vehicule_m.selectedIndex].value;
     let textSelectionnee = seclectElmtType_vehicule_m.options[seclectElmtType_vehicule_m.selectedIndex].text;
 
     if (valeurSelectionnee == "citadine") {
-        
+        console.log("Type de véhicule manuelle choisie - text = ", textSelectionnee);
+        liste_carburant.style.display = 'block';
+        listeCarburantPourCitadine.style.display = 'block';
+        //
+        listeCarburantPourCompact.style.display = 'none';
+        listeCarburantPourUtilitaire.style.display = 'none';
+        listeCarburantPourEnginChantier.style.display = 'none';
     }
     else if (valeurSelectionnee == "compact") {
-
+        console.log("Type de véhicule manuelle choisie - valeur = ", valeurSelectionnee);
+        liste_carburant.style.display = 'block';
+        listeCarburantPourCompact.style.display = 'block';
+        //
+        listeCarburantPourCitadine.style.display = 'none';
+        listeCarburantPourUtilitaire.style.display = 'none';
+        listeCarburantPourEnginChantier.style.display = 'none';
     }
     else if (valeurSelectionnee == "utilitaire") {
-        
+        console.log("Type de véhicule manuelle choisie - valeur = ", valeurSelectionnee);
+        liste_carburant.style.display = 'block';
+        listeCarburantPourUtilitaire.style.display = 'block'
+        //
+        listeCarburantPourCitadine.style.display = 'none';
+        listeCarburantPourCompact.style.display = 'none';
+        listeCarburantPourEnginChantier.style.display = 'none';
     }
     else if (valeurSelectionnee == "engin_chantier") {
-        
+        console.log("Type de véhicule manuelle choisie - valeur = ", valeurSelectionnee);
+        liste_carburant.style.display = 'block';
+        listeCarburantPourEnginChantier.style.display = 'block'
+        //
+        listeCarburantPourCitadine.style.display = 'none';
+        listeCarburantPourCompact.style.display = 'none';
+        listeCarburantPourUtilitaire.style.display = 'none';
+    }
+    else {
+        listeCarburantPourEnginChantier.style.display = 'none';
+        listeCarburantPourUtilitaire.style.display = 'none';
+        listeCarburantPourCompact.style.display = 'none';
+        listeCarburantPourCitadine.style.display = 'none';
+        liste_carburant.style.display = 'none';
     }
 });
 
+//---
+const seclectElmtTypeVehiculeAuto = document.querySelector('#type_vehicule_auto');
+seclectElmtTypeVehiculeAuto.addEventListener('change', function(){
+    // Les élément du bloc liste_carburant
+    let liste_carburant = document.querySelector('#liste_carburant');
+    // Les élément du bloc de 'liste des véhicules automatique' pour Berline
+    var listeCarburantPourBerline = document.querySelector("#liste_carburant_pour_berline");
+    // Les élément du bloc de 'liste des véhicules automatique' pour Camion
+    var listeCarburantPourCamion = document.querySelector("#liste_carburant_pour_camion");
 
+    let valeurSelectionnee = seclectElmtTypeVehiculeAuto.options[seclectElmtTypeVehiculeAuto.selectedIndex].value;
 
-
+    if (valeurSelectionnee == "berline") 
+    {
+        console.log("Type de véhicule auto choisie - valeur = ", valeurSelectionnee);
+        liste_carburant.style.display = 'block';
+        listeCarburantPourBerline.style.display = 'block';
+        //
+        listeCarburantPourCamion.style.display = 'none';
+    }
+    else if (valeurSelectionnee == "camion")
+    {
+        console.log("Type de véhicule auto choisie - valeur = ", valeurSelectionnee);
+        liste_carburant.style.display = 'block';
+        listeCarburantPourCamion.style.display = 'block';
+        //
+        listeCarburantPourBerline.style.display = 'none';   
+    }
+    else {
+        console.log("Type de véhicule auto choisie - valeur = ", valeurSelectionnee);
+        listeCarburantPourCamion.style.display = 'none';
+        listeCarburantPourBerline.style.display = 'none';   
+        liste_carburant.style.display = 'none';
+    }
+});
 
 
 
